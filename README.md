78f5efcfd1444ad0ca53cdc3a6f28890d56b5eb5
Nathaniel Smith <nathanielksmith@gmail.com>
2010-09-14 15:20:38 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+tags2json.py
+============
+
+This simple script prints a JSON representation of an audio track to STDOUT. It
+is intended for use in larger projects as a way to abstract out processing
+audio tags.
+
+It is written in Python and distributable under the GPL.
+
+It uses the [mutagen](http://code.google.com/p/mutagen/) "mutagen") library.
+
+Usage
+-----
+
+    $: tags2json.py <music file>
+    {"album": "Crystal Castles II", "title": "Doe Deer", "artist": "Crystal Castles", "genre": "Electronic", "length": "01:37", "composer": "A. Glass", "date": "2010", "tracknumber": "3"}
+
+Todo
+----
+* Support audio types other than Ogg and mp3
+
+Author
+------
+Nathaniel Smith [blog](http://chiptheglasses.com "(blog)")
---
+++ tags2json.py
@@ -0,0 +1,40 @@
+# This program is distributed under the terms 
+# of the GNU General Public License
+# Copyright 2010 Nathaniel Smith
+
+import os
+import re
+import sys
+import json
+import time
+
+from mutagen.oggvorbis import OggVorbis
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+
+if len(sys.argv) != 2:
+    print "Usage: tags2json.py filename"
+    sys.exit(1)
+
+filename = os.path.abspath( sys.argv[1] )
+
+tags = {}
+
+try:
+    if re.search(".ogg", filename, re.I):
+        audio = OggVorbis(filename)
+        tags['length'] = audio.info.length
+    elif re.search(".mp3", filename, re.I):
+        audio = EasyID3(filename)
+        tags['length'] = MP3(filename).info.length
+    else:
+        sys.exit(2)
+except:
+    sys.exit(2)
+
+tags['length'] = time.strftime('%M:%S', time.gmtime(tags['length']))
+
+for key in audio.keys():
+    tags[key] = audio[key].pop()
+
+print json.dumps(tags)
